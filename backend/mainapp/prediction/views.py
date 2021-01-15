cf456a7c2d1021ae6e2c32d5b215bb0403d2edf9
khabdrick <muhamzyali@gmail.com>
2021-01-15 12:16:48 +0100

    added prediction for permission folder

--- backend/mainapp/prediction/views.py
+++ backend/mainapp/prediction/views.py
@@ -5,9 +5,14 @@ from rest_framework.views import APIView
 from prediction.apps import PredictionConfig
 import pandas as pd
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 # Class based view to predict based on IRIS model
 class IRIS_Model_Predict(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, format=None):
         data = request.data
         keys = []
@@ -19,7 +24,7 @@ class IRIS_Model_Predict(APIView):
         loaded_mlmodel = PredictionConfig.mlmodel
         y_pred = loaded_mlmodel.predict(X)
         y_pred = pd.Series(y_pred)
-        target_map = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+        target_map = {0: "setosa", 1: "versicolor", 2: "virginica"}
         y_pred = y_pred.map(target_map).to_numpy()
         response_dict = {"Prediced Iris Species": y_pred[0]}
         return Response(response_dict, status=200)
